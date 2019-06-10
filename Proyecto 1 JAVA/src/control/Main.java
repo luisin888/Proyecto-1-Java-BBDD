9b665551363a9794837f2f8839515139f8b45f48
Edmund Home <edmundhome@hotmail.com>
2019-06-10 15:31:12 +0200

    He creado el Main

--- Proyecto 1 JAVA/src/control/Main.java
+++ Proyecto 1 JAVA/src/control/Main.java
@@ -4,10 +4,7 @@ public class Main {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-<<<<<<< HEAD
-		System.out.println("pene");
-		System.out.println();
-hjgghjghj
+
 	}
 
 }
