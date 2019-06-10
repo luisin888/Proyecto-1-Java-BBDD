551be79e4d031fc3107ace59c36d4edf34f3963e
Edmund Home <edmundhome@hotmail.com>
2019-06-10 17:26:58 +0200

    addPelicula

--- Proyecto 1 JAVA/src/datos/DatosPelicula.java
+++ Proyecto 1 JAVA/src/datos/DatosPelicula.java
@@ -1,6 +1,16 @@
 package datos;
 
+import modelo.Pelicula;
+
 public class DatosPelicula implements iDatosPelicula{
+
+	public void addPelicula(Pelicula p) {
+	
+	}
+
+	public void addPelicula() {
+
+	}
 
 	
 	
