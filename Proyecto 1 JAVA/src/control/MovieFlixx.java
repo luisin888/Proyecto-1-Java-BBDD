2074c80624fd53140267ba40c65a7e841e620855
luisin888 <51480533+luisin888@users.noreply.github.com>
2019-06-10 18:43:11 +0200

    Update MovieFlixx.java

--- Proyecto 1 JAVA/src/control/MovieFlixx.java
+++ Proyecto 1 JAVA/src/control/MovieFlixx.java
@@ -1,18 +1,90 @@
 package control;
+
+import gui.Menu;
 import utilidades.LeerTeclado;
+
 public class MovieFlixx {
-	
-	
-	
-	public int getLeerOpcion() {
-		return leerOpcion;
+
+	public MovieFlixx() {
+		super();
 	}
-	public void setLeerOpcion(int leerOpcion) {
-		this.leerOpcion = leerOpcion;
-	}
-	public int leerOpcion(){
-		LeerTeclado leer=new LeerTeclado();
-		int opc=leer.leerInt();
-		return opc;
+
+	public void seleccionarOpcion() {
+		Menu m = new Menu();
+		m.mostrarMenu();
+		LeerTeclado leer = new LeerTeclado();
+
+		int opc = leer.leerInt();
+
+		while (opc >= 1 && opc <= 12) {
+			switch (opc) {
+
+			case 1:
+				// AltaUsuariooo
+				System.out.println("caso 1");
+				break;
+
+			case 2:
+				// ModificarUsuario
+				System.out.println("caso 2");
+				break;
+
+			case 3:
+				// DarDeBajaUsuario
+				System.out.println("caso 3");
+				break;
+
+			case 4:
+				// ListarUsuarios
+				System.out.println("caso 4");
+				break;
+			case 5:
+				// CrearPeliculasNuevas
+				System.out.println("caso 5");
+				break;
+			case 6:
+				// ListarPeliculas
+				System.out.println("caso 6");
+				break;
+			case 7:
+				// ListarPeliculasPorCategoria
+				System.out.println("caso 7");
+				break;
+			case 8:
+				// ListarPeliculasMasVistas
+				System.out.println("caso 8");
+				break;
+			case 9:
+				// ListasPeliculasMejorValoradas
+				System.out.println("caso 9");
+				break;
+			case 10:
+				// ListarPeliculasVistasPorElUsuario
+				System.out.println("caso 10");
+				break;
+			case 11:
+				// BorrarPeliculasss
+				System.out.println("caso 11");
+				break;
+			case 12:
+				// ModificarPeliculas
+				System.out.println("caso 12");
+				break;
+			default:
+				break;
+			}
+			m.mostrarMenu();
+			opc = leer.leerInt();
+
 		}
+		System.out.println("************* FIN DEL PROGRAMA *************");
+	}
+
+	public int leerOpcion() {
+		LeerTeclado leer = new LeerTeclado();
+		int opc = leer.leerInt();
+		return opc;
+
+	}
+
 }
