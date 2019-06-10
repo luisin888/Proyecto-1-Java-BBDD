169873f6015aa5e77c2c21f5c3247687edd6e9f9
Edmund Home <edmundhome@hotmail.com>
2019-06-10 17:29:04 +0200

    movieflixx

---
+++ Proyecto 1 JAVA/src/control/MovieFlixx.java
@@ -0,0 +1,18 @@
+package control;
+import utilidades.LeerTeclado;
+public class MovieFlixx {
+	
+	
+	
+	public int getLeerOpcion() {
+		return leerOpcion;
+	}
+	public void setLeerOpcion(int leerOpcion) {
+		this.leerOpcion = leerOpcion;
+	}
+	public int leerOpcion{
+		LeerTeclado leer=new LeerTeclado();
+		int opc=leer.leerInt();
+		return opc;
+		}
+}
