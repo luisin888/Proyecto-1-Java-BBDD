5628a92266c4af86b83fa2b3b0e0371da3050daf
Edmund Home <edmundhome@hotmail.com>
2019-06-11 15:16:47 +0200

    Prueba calidad Servicios

--- Proyecto 1 JAVA/src/servicios/ServiciosPelicula.java
+++ Proyecto 1 JAVA/src/servicios/ServiciosPelicula.java
@@ -2,38 +2,62 @@ package servicios;
 
 import java.sql.SQLException;
 
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+
 import dao.DAOException;
 import datos.DatosPelicula;
 import datos.iDatosPelicula;
 import servicios.iServiciosPelicula;
 import modelo.Pelicula;
 
-public class ServiciosPelicula implements iServiciosPelicula{
-	
+/**
+ * 
+ * @author Raúl
+ * Nombre de clase: ConexionDB 
+ * Descripcion: Conexión con base de datos
+ * Fecha: 10/06/2019 
+ * @version: v1.0
+ *
+ */
+
+public class ServiciosPelicula implements iServiciosPelicula {
+
 	private iDatosPelicula datos = new DatosPelicula();
-	
+
 	public void addPelicula(Pelicula p) throws SQLException {
+		final Logger logger = LogManager.getLogger("Mensaje");
 		datos.addPelicula(p);
 	}
+
 	public void addPelicula() throws SQLException {
+		final Logger logger = LogManager.getLogger("Mensaje");
 		datos.addPelicula();
 	}
-	
-	public void listado(){
+
+	public void listado() {
+		final Logger logger = LogManager.getLogger("Mensaje");
 		datos.listado();
 	}
-	
-	public void eliminarPelicula(int  id)throws DAOException  {
+
+	public void eliminarPelicula(int id) throws DAOException {
+		final Logger logger = LogManager.getLogger("Mensaje");
 		datos.eliminarPelicula(id);
 	}
-	public void eliminarPelicula()throws DAOException  {
+
+	public void eliminarPelicula() throws DAOException {
+		final Logger logger = LogManager.getLogger("Mensaje");
 		datos.eliminarPelicula();
 	}
-	public void modificarPelicula(Pelicula p)throws DAOException  {
+
+	public void modificarPelicula(Pelicula p) throws DAOException {
+		final Logger logger = LogManager.getLogger("Mensaje");
 		datos.modificarPelicula(p);
 	}
-	public void modificarPelicula()throws DAOException  {
+
+	public void modificarPelicula() throws DAOException {
+		final Logger logger = LogManager.getLogger("Mensaje");
 		datos.modificarPelicula();
 	}
-	
+
 }
--- Proyecto 1 JAVA/src/servicios/iServiciosPelicula.java
+++ Proyecto 1 JAVA/src/servicios/iServiciosPelicula.java
@@ -5,15 +5,28 @@ import java.sql.SQLException;
 import dao.DAOException;
 import modelo.Pelicula;
 
+/**
+ * 
+ * @author Raúl
+ * Nombre de clase: ConexionDB 
+ * Descripcion: Conexión con base de datos
+ * Fecha: 10/06/2019 
+ * @version: v1.0
+ *
+ */
+
 public interface iServiciosPelicula {
 	public void addPelicula(Pelicula p) throws SQLException;
-	public void addPelicula()throws SQLException;
 
-	public void eliminarPelicula(int id)throws DAOException;
-	public void eliminarPelicula()throws DAOException;
+	public void addPelicula() throws SQLException;
 
-	public void modificarPelicula(Pelicula p)throws DAOException;
-	public void modificarPelicula()throws DAOException;
-	
+	public void eliminarPelicula(int id) throws DAOException;
+
+	public void eliminarPelicula() throws DAOException;
+
+	public void modificarPelicula(Pelicula p) throws DAOException;
+
+	public void modificarPelicula() throws DAOException;
+
 	public void listado();
 }
