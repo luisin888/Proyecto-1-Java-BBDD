a6be954e454361272c91107ff1d492f8cbbc1f2c
Edmund Home <edmundhome@hotmail.com>
2019-06-11 15:56:09 +0200

    Servicios usuario + prueba de calidad

--- Proyecto 1 JAVA/src/servicios/ServiciosUsuario.java
+++ Proyecto 1 JAVA/src/servicios/ServiciosUsuario.java
@@ -1,9 +1,60 @@
 package servicios;
 
-public class ServiciosUsuario {
+import java.sql.SQLException;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+import servicios.iServiciosUsuario;
+import dao.DAOException;
+import datos.DatosUsuario;
+import datos.iDatosUsuario;
+import modelo.Usuario;
 
+/**
+ * 
+ * @author Andrea
+ * Nombre de clase: ConexionDB 
+ * Descripcion: Conexi�n con base de datos
+ * Fecha: 10/06/2019 
+ * @version: v1.0
+ *
+ */
 
+public class ServiciosUsuario implements iServiciosUsuario {
 
-	
-	
+	private iDatosUsuario datos = new DatosUsuario();
+
+	public void addUsuario(Usuario u) throws SQLException {
+		final Logger logger = LogManager.getLogger("Mensaje");
+		datos.addUsuario(u);
+	}
+
+	public void addUsuario() throws SQLException {
+		final Logger logger = LogManager.getLogger("Mensaje");
+		datos.addUsuario();
+	}
+
+	public void listado() throws SQLException {
+		final Logger logger = LogManager.getLogger("Mensaje");
+		datos.listadoUsuario();
+	}
+
+	public void eliminarUsuario(int id) throws DAOException {
+		final Logger logger = LogManager.getLogger("Mensaje");
+		datos.eliminarUsuario(id);
+	}
+
+	public void eliminarUsuario() throws DAOException {
+		final Logger logger = LogManager.getLogger("Mensaje");
+		datos.eliminarUsuario();
+	}
+
+	public void modificarUsuario(Usuario u) throws DAOException {
+		final Logger logger = LogManager.getLogger("Mensaje");
+		datos.modificarUsuario(u);
+	}
+
+	public void modificarUsuario() throws DAOException {
+		final Logger logger = LogManager.getLogger("Mensaje");
+		datos.modificarUsuario();
+	}
 }
