d7fa9e1076fa46c1e2b003d394d2ed00a056742c
Edmund Home <edmundhome@hotmail.com>
2019-06-11 15:50:20 +0200

    Clase DatosUsuario donde hacemos los métodos para  * 				añadir usuario, modificar usuarios, eliminar usuarios y listar usuarios

--- Proyecto 1 JAVA/src/datos/iDatosUsuario.java
+++ Proyecto 1 JAVA/src/datos/iDatosUsuario.java
@@ -1,5 +1,36 @@
 package datos;
 
+import java.sql.SQLException;
+
+import dao.DAOException;
+import modelo.Usuario;
+
+/**
+ * @autor David
+ * Nombre de la clase: iDatosUsuario
+ * Descripcion: Clase DatosUsuario donde hacemos los métodos para
+ * 				añadir usuario, modificar usuarios, eliminar usuarios y listar usuarios
+ * Fecha: 11/06/2019
+ * 
+ * @version v1.0
+ */
+
 public interface iDatosUsuario {
+	
+	public void addUsuario(Usuario u) throws SQLException;
+
+	public void addUsuario() throws SQLException;
+
+	public Usuario encontrarUsuario(int id) throws DAOException;
+
+	public void eliminarUsuario(int id) throws DAOException;
+
+	public void eliminarUsuario() throws DAOException;
+
+	public void modificarUsuario(Usuario u) throws DAOException;
+
+	public void modificarUsuario() throws DAOException;
+
+	public void listado() throws SQLException;
 
 }
