74cf50a64cbbef0eb926305455cc888f36672877
Edmund Home <edmundhome@hotmail.com>
2019-06-12 09:51:10 +0200

    Loggers ok

--- Proyecto 1 JAVA/src/servicios/ServiciosPelicula.java
+++ Proyecto 1 JAVA/src/servicios/ServiciosPelicula.java
@@ -1,6 +1,8 @@
 package servicios;
 
 import java.sql.SQLException;
+
+import org.apache.logging.log4j.Level;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 import dao.DAOException;
@@ -22,36 +24,43 @@ public class ServiciosPelicula implements iServiciosPelicula {
 
 	public void addPelicula(Pelicula p) throws SQLException {
 		final Logger logger = LogManager.getLogger("Mensaje");
+		logger.log(Level.INFO,"Añadiendo pelicula...");
 		datos.addPelicula(p);
 	}
 
 	public void addPelicula() throws SQLException {
 		final Logger logger = LogManager.getLogger("Mensaje");
+		logger.log(Level.INFO,"Añadiendo pelicula(sobrecarga)...");
 		datos.addPelicula();
 	}
 
 	public void listado() throws SQLException {
-
+		final Logger logger = LogManager.getLogger("Mensaje");
+		logger.log(Level.INFO,"Listando peliculas...");
 		datos.listado();
 	}
 
 	public void eliminarPelicula(int id) throws DAOException {
 		final Logger logger = LogManager.getLogger("Mensaje");
+		logger.log(Level.INFO,"Eliminando pelicula...");
 		datos.eliminarPelicula(id);
 	}
 
 	public void eliminarPelicula() throws DAOException {
 		final Logger logger = LogManager.getLogger("Mensaje");
+		logger.log(Level.INFO,"Eliminando pelicula(sobrecarga)...");
 		datos.eliminarPelicula();
 	}
 
 	public void modificarPelicula(int id) throws DAOException {
 		final Logger logger = LogManager.getLogger("Mensaje");
+		logger.log(Level.INFO,"Modificando pelicula...");
 		datos.modificarPelicula(id);
 	}
 
 	public void modificarPelicula() throws DAOException {
 		final Logger logger = LogManager.getLogger("Mensaje");
+		logger.log(Level.INFO,"Modificando pelicula(sobrecarga)...");
 		datos.modificarPelicula();
 	}
 
