7546c41e5c137a143767c6f8a497ae238a7a338c
Edmund Home <edmundhome@hotmail.com>
2019-06-12 09:53:31 +0200

    loggers utilidades

--- Proyecto 1 JAVA/src/utilidades/LeerTeclado.java
+++ Proyecto 1 JAVA/src/utilidades/LeerTeclado.java
@@ -1,6 +1,8 @@
 package utilidades;
 
 import java.util.Scanner;
+
+import org.apache.logging.log4j.Level;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 
@@ -16,61 +18,72 @@ public class LeerTeclado {
 
 	public static String leerLinea() {
 		final Logger logger = LogManager.getLogger("Mensaje");
+		logger.log(Level.INFO,"Se va a leer un dato");
 		return new Scanner(System.in).nextLine();
 	}
 
 	public static String leerLinea(String mensaje) {
 		final Logger logger = LogManager.getLogger("Mensaje");
+		logger.log(Level.INFO,"Se va a leer un dato");
 		System.out.println(mensaje);
 		return leerLinea();
 	}
 
 	public static int leerInt() {
 		final Logger logger = LogManager.getLogger("Mensaje");
+		logger.log(Level.INFO,"Se va a leer un dato");
 		return new Scanner(System.in).nextInt();
 	}
 
 	public static int leerInt(String mensaje) {
 		final Logger logger = LogManager.getLogger("Mensaje");
+		logger.log(Level.INFO,"Se va a leer un dato");
 		System.out.println(mensaje);
 		return leerInt();
 	}
 
 	public static short leerShort() {
 		final Logger logger = LogManager.getLogger("Mensaje");
+		logger.log(Level.INFO,"Se va a leer un dato");
 		return new Scanner(System.in).nextShort();
 	}
 
 	public static short leerShort(String mensaje) {
 		final Logger logger = LogManager.getLogger("Mensaje");
+		logger.log(Level.INFO,"Se va a leer un dato");
 		System.out.println(mensaje);
 		return leerShort();
 	}
 
 	public static float leerFloat() {
 		final Logger logger = LogManager.getLogger("Mensaje");
+		logger.log(Level.INFO,"Se va a leer un dato");
 		return new Scanner(System.in).nextFloat();
 	}
 
 	public static float leerFloat(String mensaje) {
 		final Logger logger = LogManager.getLogger("Mensaje");
+		logger.log(Level.INFO,"Se va a leer un dato");
 		System.out.println(mensaje);
 		return leerFloat();
 	}
 
 	public static long leerLong() {
 		final Logger logger = LogManager.getLogger("Mensaje");
+		logger.log(Level.INFO,"Se va a leer un dato");
 		return new Scanner(System.in).nextLong();
 	}
 
 	public static long leerLong(String mensaje) {
 		final Logger logger = LogManager.getLogger("Mensaje");
+		logger.log(Level.INFO,"Se va a leer un dato");
 		System.out.println(mensaje);
 		return new Scanner(System.in).nextLong();
 	}
 
 	public static double leerDouble() {
 		final Logger logger = LogManager.getLogger("Mensaje");
+		logger.log(Level.INFO,"Se va a leer un dato");
 		return new Scanner(System.in).nextDouble();
 	}
 
@@ -82,22 +95,26 @@ public class LeerTeclado {
 
 	public static byte leerByte() {
 		final Logger logger = LogManager.getLogger("Mensaje");
+		logger.log(Level.INFO,"Se va a leer un dato");
 		return new Scanner(System.in).nextByte();
 	}
 
 	public static byte leerByte(String mensaje) {
 		final Logger logger = LogManager.getLogger("Mensaje");
+		logger.log(Level.INFO,"Se va a leer un dato");
 		System.out.println(mensaje);
 		return new Scanner(System.in).nextByte();
 	}
 
 	public static String leerPalabra() {
 		final Logger logger = LogManager.getLogger("Mensaje");
+		logger.log(Level.INFO,"Se va a leer un dato");
 		return new Scanner(System.in).next();
 	}
 
 	public static String leerPalabra(String mensaje) {
 		final Logger logger = LogManager.getLogger("Mensaje");
+		logger.log(Level.INFO,"Se va a leer un dato");
 		System.out.println(mensaje);
 		return new Scanner(System.in).next();
 	}
