def699e36ea8cc3baa4898d0130cb37b301a7a24
Edmund Home <edmundhome@hotmail.com>
2019-06-12 08:28:21 +0200

    sdf

--- Proyecto 1 JAVA/src/datos/DatosUsuario.java
+++ Proyecto 1 JAVA/src/datos/DatosUsuario.java
@@ -79,7 +79,7 @@ public class DatosUsuario implements iDatosUsuario {
 		final Logger logger = LogManager.getLogger("Mensaje");
 		Usuario u = encontrarUsuario(id);
 		try (Statement stmt = con.createStatement()) {
-			String query = "UPDATE usuarios SET nombrecompleto= '" + u.getNombreCompleto() + "'," + "SET fecha_nac=" + "'"
+			String query = "UPDATE usuarios" + "SET nombrecompleto= '" + u.getNombreCompleto() + "'," + "SET fecha_nac=" + "'"
 					+ u.getFechaNacimiento() + "', SET ciudad_residencia=" + "'" + u.getCiudad()+ "'" + " WHERE id_usuario=" + id;
 			if (stmt.executeUpdate(query) != 1) {
 				throw new DAOException("Error updating usuario");
